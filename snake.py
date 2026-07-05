8013fa89d63b0dcd98e1adcb76fa8bcaabb99919
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-07-05 22:00:18 +0200

    Invoke print with multiple args

--- snake.py
+++ snake.py
@@ -1,3 +1,1 @@
-print("The itsy bitsy spider\nclimbed up the waterspout.")
-print()
-print("Down came the rain\nand washed the spider out.")
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
