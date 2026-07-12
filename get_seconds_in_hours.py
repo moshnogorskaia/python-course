b361a72eee01020f9f9054e5640e68796ca32a60
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-07-12 14:21:18 +0200

    Get seconds in hours util

---
+++ get_seconds_in_hours.py
@@ -0,0 +1,9 @@
+# computes the number of seconds in a given number of hours
+
+hours = 3
+seconds_in_one_hour = 3600 # number of seconds in 1 hour
+
+print("Hours: ", hours)
+print("Seconds in Hours: ", hours * seconds_in_one_hour)
+
+print('Goodbye')
