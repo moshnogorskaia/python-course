30693b0c633f0f065502ad9ec6b5baa46ea2d429
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-07-14 22:00:02 +0200

    Use str() function

--- snake.py
+++ snake.py
@@ -1,3 +1,3 @@
-print("+" + 10 * "-" + "+")
-print(("|" + " " * 10 + "|\n") * 5, end="")
-print("+" + 10 * "-" + "+")
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is " + str((leg_a**2 + leg_b**2) ** .5))
