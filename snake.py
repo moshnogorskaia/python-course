9ae1b2dceab6cf4d300bda78a9ee365365821e48
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-07-14 21:54:54 +0200

    Concatenate strings

--- snake.py
+++ snake.py
@@ -1,6 +1,4 @@
-# This program evaluates the hypotenuse c.
-# a and b are the lengths of the legs.
-a = 3.0
-b = 4.0
-c = (a ** 2 + b ** 2) ** 0.5
-print("c =", c)
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
